4df28aa3c8ff2295d721019f7874084358a9363d
Terrence <tj.chambers@gmail.com>
2021-09-24 12:47:12 -0500

    Updated js for mobile/desktop resizing and added function to move element

--- source/themes/custom/crescenttool/scripts/libraries/region/footer.js
+++ source/themes/custom/crescenttool/scripts/libraries/region/footer.js
@@ -4,17 +4,32 @@
   Drupal.behaviors.regionFooter = {
     attach: function (context, settings) {
       var behavior_object = this;
+      var $footerHeader = $('.block-footer-navigation-block__header');
+      var $footerNav = $('.block-footer-navigation-block__nav');
+      var $footerNavContent = $footerNav.find('.block-footer-navigation-block__content');
 
+      // Move header and children to block__content above address
       if (behavior_object.isMobile()) {
-        // Move header and children to block__content above address
-        $('.block-footer-navigation-block__header').prependTo('.block-footer-navigation-block__content');
+        behavior_object.elMove($footerHeader, $footerNavContent);
       }
+
+      $(window).once('footer').on('resize', function () {
+        if (behavior_object.isMobile()) {
+          behavior_object.elMove($footerHeader, $footerNavContent);
+        }
+        else {
+          behavior_object.elMove($footerHeader, $footerNav);
+        }
+      });
     },
     isDesktop: function () {
-      return ($('body').width() >= 1024);
+      return ($('body').width() > 1024);
     },
     isMobile: function () {
-      return ($('body').width() < 1024);
+      return ($('body').width() <= 1024);
+    },
+    elMove: function ($elMove, $elMoveTo) {
+      $($elMove).prependTo($elMoveTo);
     }
   };
 
