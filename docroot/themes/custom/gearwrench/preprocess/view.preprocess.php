5e7d7976fd0a8442ee6a4ffb5b5c1f7f57e86966
Terrence <tj.chambers@gmail.com>
2021-11-05 17:19:08 -0500

    Created taxonomy style sheet, added preprocess,

--- docroot/themes/custom/gearwrench/preprocess/view.preprocess.php
+++ docroot/themes/custom/gearwrench/preprocess/view.preprocess.php
@@ -15,3 +15,11 @@
 function gearwrench_preprocess_views_view(array &$variables) {
   // Nothing to see here.
 }
+
+/**
+ * Implements hook_preprocess_views_view__taxonomy_term().
+ */
+function gearwrench_preprocess_views_view__taxonomy_term(array &$variables) {
+  // $variables['title'] = $variables['view_array']['#title']['#markup']; !
+  $variables['title'] = $variables['title_attributes']['class'];
+}
