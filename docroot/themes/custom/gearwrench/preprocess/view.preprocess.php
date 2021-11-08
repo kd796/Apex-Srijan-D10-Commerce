8c65fb2ffd9712b1486e769541b01b154e1b9744
Terrence <tj.chambers@gmail.com>
2021-11-08 10:39:01 -0600

    Added preprocess and css

--- docroot/themes/custom/gearwrench/preprocess/view.preprocess.php
+++ docroot/themes/custom/gearwrench/preprocess/view.preprocess.php
@@ -15,11 +15,3 @@
 function gearwrench_preprocess_views_view(array &$variables) {
   // Nothing to see here.
 }
-
-/**
- * Implements hook_preprocess_views_view__taxonomy_term().
- */
-function gearwrench_preprocess_views_view__taxonomy_term(array &$variables) {
-  // $variables['title'] = $variables['view_array']['#title']['#markup']; !
-  $variables['title'] = $variables['title_attributes']['class'];
-}
