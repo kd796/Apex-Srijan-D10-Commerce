035a0f6a6a0702a479e52cb36d61d96b95ab3870
Patrick Thurmond <patrick.thurmond@vmlyr.com>
2021-11-11 15:13:17 -0600

    feature/VYRKCATG-273: Class cleanup.

--- docroot/modules/custom/gearwrench_core/src/Commands/GwCliService.php
+++ docroot/modules/custom/gearwrench_core/src/Commands/GwCliService.php
@@ -2,33 +2,10 @@
 
 namespace Drupal\gearwrench_core\Commands;
 
-use Drupal\Component\Utility\Variable;
-use Drupal\Core\Cache\Cache;
-use Drupal\Core\File\FileSystemInterface;
-use Drupal\Core\Mail\MailFormatHelper;
-use Drupal\Core\Render\RenderContext;
-use Drupal\Core\Serialization\Yaml;
-use Drupal\Core\Site\Settings;
-use Drupal\file\Entity\File;
-use Drupal\webform\Controller\WebformResultsExportController;
-use Drupal\webform\Entity\Webform;
-use Drupal\webform\Entity\WebformSubmission;
-use Drupal\webform\Form\WebformResultsClearForm;
-use Drupal\webform\Form\WebformSubmissionsPurgeForm;
-use Drupal\webform\Utility\WebformObjectHelper;
-use Drupal\webform\Utility\WebformYaml;
-use Drupal\webform_submission_export_import\Form\WebformSubmissionExportImportUploadForm;
 use Drush\Commands\DrushCommands;
-use Drush\Drush;
-use Drush\Exceptions\UserAbortException;
-use League\Flysystem\FilesystemReader;
-use Psr\Log\LogLevel;
-
 use League\Flysystem\Filesystem;
 use League\Flysystem\PhpseclibV2\SftpConnectionProvider;
 use League\Flysystem\PhpseclibV2\SftpAdapter;
-use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
-use League\Flysystem\Local\LocalFilesystemAdapter;
 
 /**
  * Drush version agnostic commands.
