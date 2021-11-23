2dfc5794404073b36e20bbaa0ac5d07c23cf1e2f
Sam Nau <sam.nau@vmlyr.com>
2021-11-23 16:10:30 -0600

    VYRKCATG-294-ct-gw-update-to-brochure-images-on-cards
    - copied over teaser preprocess from GW to CT
    - updated the teaser on GW and CT to contain the thumbnail to the container and give it a black background
    - added new media page teaser image style configs

--- docroot/themes/custom/crescenttool/preprocess/node.preprocess.php
+++ docroot/themes/custom/crescenttool/preprocess/node.preprocess.php
@@ -283,6 +283,39 @@ function crescenttool_preprocess_node__product__full(array &$variables) {
   $variables['related_items'] = $main_view->buildRenderable($view_display, $main_view->args);
 }
 
+function crescenttool_preprocess_node__media_page__teaser(&$variables) {
+  /** @var \Drupal\node\NodeInterface $node */
+  $node = $variables['node'];
+  $bundle = $node->bundle();
+  $view_mode = $variables['view_mode'];
+
+  $bundle_css = Html::cleanCssIdentifier($bundle);
+  $view_mode_css = Html::cleanCssIdentifier($view_mode);
+
+  // Track variables that should be converted to attribute objects.
+  $variables['#attribute_variables'][] = 'media_attributes';
+
+  $variables['inner_attributes']['class'][] = 'node__inner';
+  $variables['media_attributes']['class'][] = 'node__media';
+
+  // Move media to media variable.
+  if (isset($variables['content']['field_media'][0])) {
+    $variables['media_attributes']['class'][] = 'node__media--with-media';
+    $variables['media_attributes']['class'][] = 'node__listing-image';
+    $variables['media'] = $variables['content']['field_media'];
+    unset($variables['media']['#theme']);
+    unset($variables['content']['field_media']);
+  }
+  else {
+    $variables['media_attributes']['class'][] = 'node__media--no-media';
+  }
+
+  if (!empty($variables['content']['field_preferred_listing_image'][0])) {
+    $variables['media'] = $variables['content']['field_preferred_listing_image'];
+    unset($variables['content']['field_preferred_listing_image']);
+  }
+}
+
 /**
  * Implements hook_preprocess_node__BUNDLE__VIEW_MODE() for search index.
  */
