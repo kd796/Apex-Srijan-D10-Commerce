efa6144c1af4d5881711c9c5302ed20e031fea77
Jude Mcculley <jude.mcculley@vmlyr.com>
2021-12-06 08:29:24 -0600

    Added missing files from GW to CT and changed class names for Buy Now Sticky

---
+++ docroot/modules/custom/crescenttool_core/src/Plugin/Block/ProductBuyNowSticky.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\gearwrench_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\node\NodeInterface;
+
+/**
+ * Provides a search block for the header.
+ *
+ * @Block(
+ *   id = "product_buy_now_sticky",
+ *   admin_label = @Translation("Product: Buy Now Sticky"),
+ *   category = @Translation("Crescenttool Core")
+ * )
+ */
+class ProductBuyNowSticky extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $title = NULL;
+    $sku = NULL;
+    $node = \Drupal::routeMatch()->getParameter('node');
+    if ($node instanceof NodeInterface) {
+      // You can get nid and anything else you need from the node object.
+      $title = $node->get('field_long_description')->value;
+      $sku = $node->get('title')->value;
+    }
+
+    return [
+      '#title' => $title,
+      '#sku' => $sku,
+      '#theme' => 'ct_product_buy_now_sticky',
+      '#cache' => [
+        'max-age' => 0
+      ],
+    ];
+  }
+
+}
