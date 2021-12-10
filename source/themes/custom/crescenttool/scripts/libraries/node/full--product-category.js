f508ba79af550f8eac163d669b53e8ba945d7a64
Terrence <tj.chambers@gmail.com>
2021-12-10 12:51:56 -0600

    undid full product change

--- source/themes/custom/crescenttool/scripts/libraries/node/full--product-category.js
+++ source/themes/custom/crescenttool/scripts/libraries/node/full--product-category.js
@@ -235,6 +235,7 @@
         var cleanedRadioArray = [];
         var checkedFilters = $('input[type=checkbox]:checked');
         var checkedRadios = $('input[type=radio]:checked');
+        var setText = '';
 
         // Loop through all checked filters
         for (var indx = 0; indx < checkedFilters.length; indx++) {
