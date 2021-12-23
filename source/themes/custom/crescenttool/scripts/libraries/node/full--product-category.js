bd0f0ecd51419b069b3a2ffab642efb8f6116dd5
Patrick Thurmond <patrick.thurmond@vmlyr.com>
2021-12-23 13:39:50 -0600

    VYRKCATG-326: Fixed the shown icons in mobile.

--- source/themes/custom/crescenttool/scripts/libraries/node/full--product-category.js
+++ source/themes/custom/crescenttool/scripts/libraries/node/full--product-category.js
@@ -212,7 +212,7 @@
 
       // Logic for mobile filtering menu.
       if (window.innerWidth <= 768) {
-        var $mobileMenuIcon = $('.sort-icon');
+        var $mobileMenuIcon = $('.filter-icon');
         var $mobileCloseIcon;
         var $mobileCategoryFilters = $('.crescenttool-product-category-filters');
         var $viewHeader = $('.view-header');
