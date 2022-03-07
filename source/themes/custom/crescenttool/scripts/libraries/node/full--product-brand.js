44282b080ac407f8e6d05e2275f349bed0bab9b2
Patrick Thurmond <patrick.thurmond@vmlyr.com>
2022-03-07 13:18:30 -0600

    VYRKCATG-373: Fixed problem with the pop to top of products on the brand page.

--- source/themes/custom/crescenttool/scripts/libraries/node/full--product-brand.js
+++ source/themes/custom/crescenttool/scripts/libraries/node/full--product-brand.js
@@ -3,7 +3,7 @@
 
   Drupal.behaviors.componentProductBrand = {
     attach: function (context, settings) {
-      $('.view-product-category:not(.view-product-category--js-initialized)').once('product-brand-filter').each(function (index) {
+      $('.node--type-product-brand').once().each(function (index) {
         // Scroll down to the products when loading the category page.
         $('.view-product-category')[0].scrollIntoView();
       });
