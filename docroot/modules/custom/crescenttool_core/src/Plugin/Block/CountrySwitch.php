e8c4ea8b86aa6d11b578a82d90bbbc060aebcd78
Patrick Thurmond <patrick.thurmond@vmlyr.com>
2022-06-27 16:52:50 -0500

    Fixing country switcher.

--- docroot/modules/custom/crescenttool_core/src/Plugin/Block/CountrySwitch.php
+++ docroot/modules/custom/crescenttool_core/src/Plugin/Block/CountrySwitch.php
@@ -19,11 +19,13 @@ class CountrySwitch extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    $united_states_url = 'https://www.crescenttool.com';
     $australia_url = 'http://www.crescenttool.com.au/';
     $mexico_url = 'http://www.crescenttool.com.mx/';
     $eu_url = 'http://www.crescenttool.eu/';
 
     return [
+      'united_states_url' => $united_states_url,
       'australia_url' => $australia_url,
       'mexico_url' => $mexico_url,
       'eu_url' => $eu_url,
