970e58f76019d4ebb1f95d8260f98de5bbf97313
Patrick Thurmond <patrick.thurmond@vmlyr.com>
2022-07-05 10:37:33 -0500

    sata_sites: Initial setup of the SATA site files.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -103,3 +103,36 @@ $sites['dev-www.crescenttool.com.au'] = 'crescenttool_au';
 $sites['stg-www.crescenttool.com.au'] = 'crescenttool_au';
 $sites['prod-www.crescenttool.com.au'] = 'crescenttool_au';
 $sites['www.crescenttool.com.au'] = 'crescenttool_au';
+
+/**
+ * SATA Sites
+ * -----------------
+ */
+
+// SATA Colombia
+$sites['sata.com.co'] = 'sata_colombia';
+$sites['www.sata.com.co'] = 'sata_colombia';
+$sites['prod-www.sata.com.co'] = 'sata_colombia';
+$sites['stg-www.sata.com.co'] = 'sata_colombia';
+$sites['dev-www.sata.com.co'] = 'sata_colombia';
+
+// SATA Colombia
+$sites['sataferramentas.com.br'] = 'sata_brazil';
+$sites['www.sataferramentas.com.br'] = 'sata_brazil';
+$sites['prod-www.sataferramentas.com.br'] = 'sata_brazil';
+$sites['stg-www.sataferramentas.com.br'] = 'sata_brazil';
+$sites['dev-www.sataferramentas.com.br'] = 'sata_brazil';
+
+// SATA Colombia
+$sites['satatools.eu'] = 'sata_emea';
+$sites['www.satatools.eu'] = 'sata_emea';
+$sites['prod-www.satatools.eu'] = 'sata_emea';
+$sites['stg-www.satatools.eu'] = 'sata_emea';
+$sites['dev-www.satatools.eu'] = 'sata_emea';
+
+// SATA Colombia
+$sites['satatools.us'] = 'sata_us';
+$sites['www.satatools.us'] = 'sata_us';
+$sites['prod-www.satatools.us'] = 'sata_us';
+$sites['stg-www.satatools.us'] = 'sata_us';
+$sites['dev-www.satatools.us'] = 'sata_us';
