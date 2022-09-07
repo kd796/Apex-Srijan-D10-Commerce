c15f6222c2aabb639c218490e2995d24b62bfa90
Terrence Chambers <terrence.chambers@vmlyr.com>
2022-09-07 14:14:01 -0500

    Added class conditional to preprocess, removed js no longer needed, changed bg color and padding styling back to css

--- source/themes/custom/sata/scripts/libraries/paragraph/full--callout-diagram.js
+++ source/themes/custom/sata/scripts/libraries/paragraph/full--callout-diagram.js
@@ -5,27 +5,10 @@
     attach: function (context, settings) {
       $('.component-callout-diagram:not(.component-callout-diagram--js-initialized)').each(function (index) {
         var $component = $(this);
-        var $diagramItemContainer = $component.find('.field--name-field-callout-diagram-item > .field__item');
-        var $diagramItemContainerNum = $diagramItemContainer.length;
-        var $diagramBottomContainer = $component.find('.component-callout-diagram__bottom-container');
-        var $diagramBottomContainerWidth = '100%';
+        // var $diagramItemContainer = $component.find('.field--name-field-callout-diagram-item > .field__item');
 
         // Track that this component has been initialized.
         $component.addClass('component-hero--js-initialized');
-
-        // Set width of bottom container based on num of item containers
-        if ($diagramItemContainerNum === 1) {
-          $diagramBottomContainerWidth = '25%';
-        }
-        else if ($diagramItemContainerNum === 2) {
-          $diagramBottomContainerWidth = '50%';
-        }
-        else if ($diagramItemContainerNum === 3) {
-          $diagramBottomContainerWidth = '75%';
-        }
-        console.log($diagramBottomContainerWidth);
-
-        $($diagramBottomContainer).css('width', $diagramBottomContainerWidth);
       });
     }
   };
