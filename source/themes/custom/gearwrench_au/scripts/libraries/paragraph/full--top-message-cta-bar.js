0fc8cf1f33500a7828d9ecfa3549020d3cd6d3b7
Terrence Chambers <terrence.chambers@vmlyr.com>
2022-10-26 11:45:37 -0500

    Added CT AU & GW US & AU top message cta bar js and css, Updated product sticky nav css values to variables, fixed sata theme conditional, SATA fixed mobile menu link hover color

---
+++ source/themes/custom/gearwrench_au/scripts/libraries/paragraph/full--top-message-cta-bar.js
@@ -0,0 +1,38 @@
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.topMessageCTABar = {
+    attach: function (context, settings) {
+      var lastScrollTop = 0;
+      var topMessageCTABar = $('.component-top-message-cta-bar__content');
+      var topofDiv = $('.block-system-main-block').offset().top;
+
+      // On scroll down, header and expanded navigation disappears, scroll up re-appears, logo area resizes.
+      window.addEventListener('scroll', function () {
+        var st = $(this).scrollTop();
+        topofDiv = $('.block-system-main-block').offset().top;
+
+        if (window.scrollY > topofDiv) {
+          if (st > lastScrollTop) {
+            topMessageCTABar.removeClass('show--with-nav');
+            if (!topMessageCTABar.hasClass('show--no-nav')) {
+              topMessageCTABar.addClass('show--no-nav');
+            }
+          }
+          else {
+            topMessageCTABar.removeClass('show--no-nav');
+            if (!topMessageCTABar.hasClass('show--with-nav')) {
+              topMessageCTABar.addClass('show--with-nav');
+            }
+          }
+        }
+        else {
+          topMessageCTABar.removeClass('show--no-nav');
+          topMessageCTABar.removeClass('show--with-nav');
+        }
+        lastScrollTop = st;
+      });
+    }
+  };
+
+})(jQuery, Drupal);
