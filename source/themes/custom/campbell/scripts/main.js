7902e693b793bae6bab68c8a0bc25558d5a4d0e4
vashantha deepika <vasantha.deepika@srijan.net>
2023-02-21 18:26:43 +0530

    APEX-187: Done the Product Landing Page.

--- source/themes/custom/campbell/scripts/main.js
+++ source/themes/custom/campbell/scripts/main.js
@@ -62,35 +62,6 @@
             e.stopPropagation();
           });
       });
-      // slider.
-      $(function () {
-        var homeSlider = $('.slider');
-        var activeLink = $('.home-text');
-        homeSlider.once('homeSlider').owlCarousel({
-          items: 1,
-          loop: false,
-          rewind: true,
-          margin: 10,
-          autoplay: 5000,
-          nav: false,
-          dots: false,
-          animateOut: 'fadeOut',
-          animateIn: 'fadeIn'
-        });
-        activeLink.once('activeLink').owlCarousel({
-          items: 1,
-          loop: false,
-          rewind: true,
-          autoplay: false,
-          nav: false,
-          dots: false,
-          animateOut: 'fadeOut',
-          animateIn: 'fadeIn'
-        });
-        homeSlider.once('homeSliderChange').on('changed.owl.carousel', function (e) {
-          activeLink.trigger('next.owl.carousel');
-        });
-      });
 
       /* end */
     }
