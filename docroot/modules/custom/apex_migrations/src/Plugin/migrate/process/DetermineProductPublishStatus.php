6f80229665c27eed89c185c4ed286512c49fe85c
Aditya Singh <aditya.singh@srijan.net>
2023-04-24 09:56:51 +0530

    APS-83 Cleanup of Discontinued Obsolete Products

---
+++ docroot/modules/custom/apex_migrations/src/Plugin/migrate/process/DetermineProductPublishStatus.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\apex_migrations\Plugin\migrate\process;
+
+use Drupal\Core\Logger\LoggerChannelTrait;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Unpublish discontinued products.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "apex_determine_published_status"
+ * )
+ */
+class DetermineProductPublishStatus extends ProcessPluginBase {
+  use LoggerChannelTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $values = $this->configuration['value'];
+    $published = TRUE;
+
+    if (empty($values) || empty($value)) {
+      return $published;
+    }
+
+    // If $value matches any of the configured values, unpublish the node.
+    foreach ($values as $val) {
+      if ($value !== $val) {
+        continue;
+      }
+      $this->getLogger('apex_migrations')->notice("Product: @product is unpublished as it is discontinued.", ['@product' => $row->getIdMap()['sourceid1']]);
+      $published = FALSE;
+    }
+
+    return $published;
+  }
+
+}
