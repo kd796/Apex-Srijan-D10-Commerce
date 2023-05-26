26c3e42e6a614dde01d565f9cce4b4c9324a2ec5
jitender-star <jitender.kumar@srijan.net>
2023-05-26 12:44:31 +0530

    Apex-311: Share feature added.

---
+++ docroot/sites/apex-tools/modules/custom/addtothis_apex_tool/src/Plugin/Block/AddthisBlock.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\addtothis_apex_tool\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Provides an addthis block.
+ *
+ * @Block(
+ *   id = "addtothis_apex_tool_custom",
+ *   admin_label = @Translation("AddThis Share"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class AddthisBlock extends BlockBase {
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $addthis_content = "<a href='https://www.addthis.com/bookmark.php' class='addthis_button' style='text-decoration:none;' target='_blank'>
+    <img src='/sites/apex-tools/files/2023-05/share-grey.png' width='16' height='16' border='0'/>" . $this->t('Share') . "</a>
+    ";
+
+    return [
+      '#theme' => 'add_to_this_content',
+      '#addthis_content' => $addthis_content,
+    ];
+  }
+
+}
