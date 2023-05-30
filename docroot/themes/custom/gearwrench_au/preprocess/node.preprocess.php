429a9a5ad5a266fd1f02b7bf15628c57bd65f3e0
Muneeb Mukhtar <muneeb.mukhtar@srijan.net>
2023-05-30 15:53:14 +0530

    fix phpcs errors

--- docroot/themes/custom/gearwrench_au/preprocess/node.preprocess.php
+++ docroot/themes/custom/gearwrench_au/preprocess/node.preprocess.php
@@ -284,7 +284,7 @@ function gearwrench_au_preprocess_node__product__full(array &$variables) {
   // Show the RRP to the users accessing from AU only.
   $geo = (array_key_exists('HTTP_X_GEO_COUNTRY', $_SERVER)) ? $_SERVER['HTTP_X_GEO_COUNTRY'] : "";
   if (!empty($geo) && $geo == "AU" && !empty($node->field_rrp->value)) {
-    $variables['rrp'] = number_format((float) $node->field_rrp->value, 2);;
+    $variables['rrp'] = number_format((float) $node->field_rrp->value, 2);
   }
   // Product Features.
   $page_top_products_features = $variables['content']['field_product_features'];
