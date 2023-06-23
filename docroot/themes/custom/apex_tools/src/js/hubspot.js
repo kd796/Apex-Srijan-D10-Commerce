68c41da8ffa45a350cd4d1c4b0647c1718d4a270
Kuntal <kuntal.das@srijan.net>
2023-06-23 12:52:20 +0530

    APEX-330: Hubspot form js change

--- docroot/themes/custom/apex_tools/src/js/hubspot.js
+++ docroot/themes/custom/apex_tools/src/js/hubspot.js
@@ -1,32 +1,29 @@
-(function ($) {
-  var $contentlist = $('.hubspot-tabs-content li');
-  var $tabslist = $('.hubspot-tabs li');
+jQuery(document).ready(function () {
+  var jQuerycontentlist = jQuery('.hubspot-tabs-content li');
+  var jQuerytabslist = jQuery('.hubspot-tabs li');
 
-  $('.hubspot-tabs').once('hubspot-tabs').on('click', 'li', function (e) {
-    var $current = $(this);
-    var index = $current.index();
-
-    if ($current.hasClass('active-tab')) {
-      $current.removeClass('active-tab');
+  jQuery('.hubspot-tabs').once('hubspot-tabs').on('click', 'li', function (e) {
+    var jQuerycurrent = jQuery(this);
+    var index = jQuerycurrent.index();
+    if (jQuerycurrent.hasClass('active-tab')) {
+      jQuerycurrent.removeClass('active-tab');
     } else {
-      $tabslist.removeClass('active-tab');
-      $current.addClass('active-tab');
+      jQuerytabslist.removeClass('active-tab');
+      jQuerycurrent.addClass('active-tab');
     }
 
-    if ($contentlist.eq(index).hasClass('active')) {
-      $contentlist.eq(index).removeClass('active');
+    if (jQuerycontentlist.eq(index).hasClass('active')) {
+      jQuerycontentlist.eq(index).removeClass('active');
     } else {
-      $contentlist.removeClass('active');
-      $contentlist.eq(index).addClass('active');
-      var target = $($(this).find('.checkable-card').attr('href'));
+      jQuerycontentlist.removeClass('active');
+      jQuerycontentlist.eq(index).addClass('active');
+      var target = jQuery(jQuery(this).find('.checkable-card').attr('href'));
       if (target.length) {
-        $('html, body').animate({
+        jQuery('html, body').animate({
           scrollTop: target.offset().top
         }, 500);
       }
     }
     return false;
   });
-
-  // end
-})(jQuery);
+});
