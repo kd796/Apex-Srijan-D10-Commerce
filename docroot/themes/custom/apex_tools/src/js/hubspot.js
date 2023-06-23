d30e380bb29b13432995f6530a53f092d80eae55
Kuntal <kuntal.das@srijan.net>
2023-06-23 10:36:36 +0530

    Apex-330: Hubspot js changes

--- docroot/themes/custom/apex_tools/src/js/hubspot.js
+++ docroot/themes/custom/apex_tools/src/js/hubspot.js
@@ -1,38 +1,32 @@
-(function ($, Drupal) {
-  'use strict';
+(function ($) {
+  var $contentlist = $('.hubspot-tabs-content li');
+  var $tabslist = $('.hubspot-tabs li');
 
-  Drupal.behaviors.hubspot = {
-    attach: function (context, settings) {
-      var $contentlist = $('.hubspot-tabs-content li');
-      var $tabslist = $('.hubspot-tabs li');
+  $('.hubspot-tabs').once('hubspot-tabs').on('click', 'li', function (e) {
+    var $current = $(this);
+    var index = $current.index();
 
-      $('.hubspot-tabs', context).once('hubspot-tabs').on('click', 'li', function (e) {
-        var $current = $(this);
-        var index = $current.index();
-
-        if ($current.hasClass('active-tab')) {
-          $current.removeClass('active-tab');
-        } else {
-          $tabslist.removeClass('active-tab');
-          $current.addClass('active-tab');
-        }
-
-        if ($contentlist.eq(index).hasClass('active')) {
-          $contentlist.eq(index).removeClass('active');
-        } else {
-          $contentlist.removeClass('active');
-          $contentlist.eq(index).addClass('active');
-          var target = $($(this).find('.checkable-card').attr('href'));
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 500);
-          }
-        }
-        return false;
-      });
-
-      // end
+    if ($current.hasClass('active-tab')) {
+      $current.removeClass('active-tab');
+    } else {
+      $tabslist.removeClass('active-tab');
+      $current.addClass('active-tab');
     }
-  };
-})(jQuery, Drupal);
+
+    if ($contentlist.eq(index).hasClass('active')) {
+      $contentlist.eq(index).removeClass('active');
+    } else {
+      $contentlist.removeClass('active');
+      $contentlist.eq(index).addClass('active');
+      var target = $($(this).find('.checkable-card').attr('href'));
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+      }
+    }
+    return false;
+  });
+
+  // end
+})(jQuery);
