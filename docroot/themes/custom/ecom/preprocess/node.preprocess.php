6f46856d8f9b0d39484097c7ea1282f85793cc05
Kuntal <kuntal.das@srijan.net>
2023-08-17 13:50:37 +0530

    APEX-832: Make price and cart dynamic

--- docroot/themes/custom/ecom/preprocess/node.preprocess.php
+++ docroot/themes/custom/ecom/preprocess/node.preprocess.php
@@ -100,7 +100,16 @@ function ecom_preprocess_node__product__full(array &$variables) {
   /** @var Drupal\node\NodeInterface $node */
   $node = $variables['elements']['#node'];
   $entity_type_manager = \Drupal::entityTypeManager();
-
+  if ($node->field_commerce_product != NULL) {
+    $prod_variation_obj = $node->field_commerce_product->entity->variations->entity;
+    // Price.
+    $price = $prod_variation_obj->getPrice();
+    // Variance id.
+    $var_id = $prod_variation_obj->id();
+    // Passing in template.
+    $variables['variation_id'] = $var_id;
+    $variables['price'] = $price;
+  }
   // Count Product Images.
   $variables['product_images'] = NULL;
 
