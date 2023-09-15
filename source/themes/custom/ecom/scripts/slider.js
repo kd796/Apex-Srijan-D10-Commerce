001bcf4f544631d445c370f2771cbcce9045178b
vashantha deepika <vasantha.deepika@srijan.net>
2023-09-15 13:22:04 +0530

    APEX-721: Implementation of featured products section

--- source/themes/custom/ecom/scripts/slider.js
+++ source/themes/custom/ecom/scripts/slider.js
@@ -5,31 +5,22 @@
     attach: function (context, settings) {
       // slider.
       $(function () {
-        var homeSlider = $('.slider');
-        var activeLink = $('.home-text');
-        $(once('homeSlider', homeSlider, context)).owlCarousel({
-          items: 1,
-          loop: false,
-          rewind: true,
-          margin: 10,
-          autoplay: 5000,
-          nav: false,
-          dots: false,
-          animateOut: 'fadeOut',
-          animateIn: 'fadeIn'
-        });
-        $(once('activeLink', activeLink, context)).owlCarousel({
-          items: 1,
-          loop: false,
-          rewind: true,
-          autoplay: false,
-          nav: false,
-          dots: false,
-          animateOut: 'fadeOut',
-          animateIn: 'fadeIn'
-        });
-        $(once('homeSliderChange', homeSlider, context)).on('changed.owl.carousel', function (e) {
-          activeLink.trigger('next.owl.carousel');
+        // eslint-disable-next-line no-unused-vars, no-undef
+        const featuredProducts = new Swiper('.featured-products', {
+          slidesPerView: 2,
+          navigation: {
+            nextEl: '.feature-products-next',
+            prevEl: '.feature-products-prev'
+          },
+          watchSlidesVisibility: true,
+          breakpoints: {
+            766.9: {
+              slidesPerView: 3
+            },
+            959: {
+              slidesPerView: 5
+            }
+          }
         });
       });
 
