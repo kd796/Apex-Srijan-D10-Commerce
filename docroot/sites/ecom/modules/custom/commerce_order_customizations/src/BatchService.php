1621dfd5b78323113c524b3d0d8a017df5f7fba4
Kuntal <kuntal.das@srijan.net>
2023-09-22 21:34:52 +0530

    APEX-763: Order update

---
+++ docroot/sites/ecom/modules/custom/commerce_order_customizations/src/BatchService.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Drupal\commerce_order_customizations;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Class BatchService for Batch Process.
+ */
+class BatchService {
+  use StringTranslationTrait;
+
+  /**
+   * Batch process callback.
+   *
+   * @param object $xml_file
+   *   File object.
+   * @param string $ftp_folder
+   *   Ftp folder name.
+   * @param object $context
+   *   Context for operations.
+   */
+  public static function processOrderUpdate($xml_file, $ftp_folder, &$context = []) {
+
+    $expanded_path = explode('/', $xml_file->uri);
+    $xml_data = file_get_contents($xml_file->uri);
+    if ($xml_data != "") {
+      $xmlObj = simplexml_load_string($xml_data);
+      $orders_details['order_number'] = '';
+      $orders_details['sap_order_number'] = '';
+      $orders_details['shipment_date'] = '';
+      $orders_details['shipment_number'] = '';
+      $orders_details['carrier_name'] = '';
+      $orders_details['carrier_tracking_number'] = [];
+      $orders_details['item_and_quantity'] = [];
+      $orders_details['total_quantity'] = 0;
+      // order_number.
+      foreach ($xmlObj->IDOC->E1EDL20->E1EDL24 as $element) {
+        if ((string) $element->E1EDL41->QUALI === '001') {
+          $orders_details['order_number'] = (string) $element->E1EDL41->BSTNR;
+          break;
+        }
+      }
+      // sap_order_number.
+      foreach ($xmlObj->IDOC->E1EDL20->E1EDL24 as $element) {
+        if ((string) $element->E1EDL43->QUALF === 'C') {
+          $orders_details['sap_order_number'] = (string) $element->E1EDL43->BELNR;
+          break;
+        }
+      }
+      // shipment_date.
+      foreach ($xmlObj->IDOC->E1EDL20->E1EDT13 as $element) {
+        if ((string) $element->QUALF === '006') {
+          $orders_details['shipment_date'] = (string) $element->ISDD;
+          break;
+        }
+      }
+      // shipment_number.
+      $orders_details['shipment_number'] = (string) $xmlObj->IDOC->E1EDL20->VBELN;
+      // carrier_name.
+      $orders_details['carrier_name'] = (string) $xmlObj->IDOC->E1EDL20->E1EDL28->E1EDL29->ROUTE_BEZ;
+      // carrier_tracking_number.
+      foreach ($xmlObj->IDOC->E1EDL20->E1EDL18 as $element) {
+        if ((string) $element->QUALF === 'PRO') {
+          $orders_details['carrier_tracking_number'][] = (string) $element->PARAM;
+          break;
+        }
+      }
+      foreach ($xmlObj->IDOC->E1EDL20->E1EDL37 as $element) {
+        if ($element->EXIDV2) {
+          $orders_details['carrier_tracking_number'][] = trim((string) $element->EXIDV2);
+        }
+      }
+      // material_number(Mostly SKU number)
+      // quantity_shipped.
+      foreach ($xmlObj->IDOC->E1EDL20->E1EDL24 as $element) {
+        $item_sku = trim((string) $element->MATNR);
+        $qty_shipped = trim((string) $element->LFIMG);
+        $orders_details['item_and_quantity'][$item_sku] = $qty_shipped;
+      }
+      // Calculate total items.
+      foreach ($orders_details['item_and_quantity'] as $sku => $qty) {
+        $orders_details['total_quantity'] = $orders_details['total_quantity'] + (int) $qty;
+      }
+      $create_shipment_status = \Drupal::service('commerce_order_customizations.utility')->createShipment($orders_details);
+      if ($create_shipment_status) {
+        $order_update_status = \Drupal::service('commerce_order_customizations.utility')->orderStatusUpdate($orders_details);
+        if ($order_update_status) {
+          \Drupal::logger('commerce_order_customizations')->notice("New shipment created and order is updated for Order Number: '{$orders_details['order_number']}'");
+          \Drupal::service('commerce_order_customizations.utility')->deleteFtpFiles(array_pop($expanded_path), $ftp_folder);
+        }
+      }
+      else {
+        \Drupal::logger('commerce_order_customizations')->notice("No new shipment created for Order Number: '{$orders_details['order_number']}'");
+        \Drupal::service('commerce_order_customizations.utility')->deleteFtpFiles(array_pop($expanded_path), $ftp_folder);
+      }
+    }
+    else {
+      $file_name = array_pop($expanded_path);
+      \Drupal::logger('commerce_order_customizations')->error("XML file: '{$file_name}' had no data");
+      \Drupal::service('commerce_order_customizations.utility')->deleteFtpFiles(array_pop($expanded_path), $ftp_folder);
+    }
+  }
+
+  /**
+   * Batch Finished callback.
+   *
+   * @param bool $success
+   *   Success of the operation.
+   * @param array $results
+   *   Array of results for post processing.
+   * @param array $operations
+   *   Array of operations.
+   */
+  public static function processUserUpdateFinished($success, array $results, array $operations) {
+    $messenger = \Drupal::messenger();
+    if ($success) {
+
+      \Drupal::logger('commerce_order_customizations')->notice(t('Successfully processed All the files'));
+    }
+    else {
+      // An error occurred.
+      // $operations contains the operations that remained unprocessed.
+      $error_operation = reset($operations);
+      $messenger->addMessage(
+        $this->t('An error occurred while processing @operation with arguments : @args',
+          [
+            '@operation' => $error_operation[0],
+            '@args' => print_r($error_operation[0], TRUE),
+          ]
+        )
+      );
+    }
+  }
+
+}
--- docroot/sites/ecom/modules/custom/commerce_order_customizations/src/Commands/SapOrderUpdate.php
+++ docroot/sites/ecom/modules/custom/commerce_order_customizations/src/Commands/SapOrderUpdate.php
@@ -3,14 +3,12 @@
 namespace Drupal\commerce_order_customizations\Commands;
 
 use Drupal\commerce_order_customizations\UtilityOrder;
-use Drupal\commerce_shipping\Entity\Shipment;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\paragraphs\Entity\Paragraph;
 use Drush\Commands\DrushCommands;
+use League\Flysystem\FileAttributes;
 use League\Flysystem\Filesystem;
 use League\Flysystem\PhpseclibV2\SftpAdapter;
 use League\Flysystem\PhpseclibV2\SftpConnectionProvider;
@@ -123,20 +121,23 @@ class SapOrderUpdate extends DrushCommands {
     if ($connection_status != 0) {
       $public_folder_path = 'public://import/order_update_xml_files/';
       $folder_path = $this->drupalFileSystem->realpath($public_folder_path);
-      $remoteFiles = $this->filesystem->listContents($this->root);
-      // dump(($remoteFiles));
-      // dump($this->root);.
-      foreach ($remoteFiles as $remoteFile) {
+      $remoteFiles = $this->filesystem->listContents($this->root)->toArray();
+      if (count($remoteFiles) != 0) {
+        foreach ($remoteFiles as $remoteFile) {
 
-        $this->downloadAndStoreFile($remoteFile['path'], $public_folder_path);
-        $counter++;
+          $this->downloadAndStoreFile($remoteFile, $public_folder_path);
+          $counter++;
+        }
+        $this->logger()->notice('Download and stored phase from FTP Completed');
+        $this->logger()->notice("Total number of files downloaded are: '{$counter}'");
+        // Getting order details array after processing the xml files and creating shipments.
+        $data = $this->customXmlReader($public_folder_path);
+        // Deleting all the files from public folder.
+        $this->deleteLocalFiles($folder_path);
       }
-      $this->logger()->notice('Download and stored phase from FTP Completed');
-      $this->logger()->notice("Total number of files processed are: '{$counter}'");
-      // Getting order details array after processing the xml files and creating shipments.
-      $data = $this->customXmlReader($public_folder_path);
-      // Deleting all the files from public folder.
-      $this->deleteLocalFiles($folder_path);
+      else {
+        $this->logger()->notice('There are no Files for Order Update at this moment');
+      }
     }
     else {
       $this->output()->writeln("Please check the FTP credential form");
@@ -182,18 +183,19 @@ class SapOrderUpdate extends DrushCommands {
   /**
    * Download the file from FTP and store it in the local directory.
    *
-   * @param string $remotePath
-   *   The path to the folder ftp server.
+   * @param \League\Flysystem\FileAttributes $remoteFile
+   *   The file object.
    * @param string $localPath
    *   The local public folder path.
    */
-  public function downloadAndStoreFile($remotePath, $localPath) {
+  public function downloadAndStoreFile(FileAttributes $remoteFile, $localPath) {
     // Defining Public Folder path.
+    $remotePath = $remoteFile->path();
     $expanded_path = explode('/', $remotePath);
     $simple_filename = array_pop($expanded_path);
     $file_resource = $this->filesystem->read($remotePath);
     $this->drupalFileSystem->saveData($file_resource, $localPath . $simple_filename);
-    $this->output()->writeln("File '{$remotePath}' successfully downloaded and stored.");
+    $this->logger()->success("File '{$remotePath}' successfully downloaded and stored.");
   }
 
   /**
@@ -205,171 +207,24 @@ class SapOrderUpdate extends DrushCommands {
   public function customXmlReader($localPath) {
 
     $xml_files = $this->drupalFileSystem->scanDirectory($localPath, '/\.xml$/');
-    $data = [];
     if (!empty($xml_files)) {
-      try {
-        foreach ($xml_files as $xml_file) {
-          $xml_data = file_get_contents($xml_file->uri);
-          $xmlObj = simplexml_load_string($xml_data);
-          $orders_details['carrier_tracking_number'] = [];
-          $orders_details['item_and_quantity'] = [];
-          $orders_details['total_quantity'] = 0;
-          // order_number.
-          foreach ($xmlObj->IDOC->E1EDL20->E1EDL24 as $element) {
-            if ((string) $element->E1EDL41->QUALI === '001') {
-              $orders_details['order_number'] = (string) $element->E1EDL41->BSTNR;
-              break;
-            }
-          }
-          // sap_order_number.
-          foreach ($xmlObj->IDOC->E1EDL20->E1EDL24 as $element) {
-            if ((string) $element->E1EDL43->QUALF === 'C') {
-              $orders_details['sap_order_number'] = (string) $element->E1EDL43->BELNR;
-              break;
-            }
-          }
-          // shipment_date.
-          foreach ($xmlObj->IDOC->E1EDL20->E1EDT13 as $element) {
-            if ((string) $element->QUALF === '006') {
-              $orders_details['shipment_date'] = (string) $element->ISDD;
-              break;
-            }
-          }
-          // shipment_number.
-          $orders_details['shipment_number'] = (string) $xmlObj->IDOC->E1EDL20->VBELN;
-          // carrier_name.
-          $orders_details['carrier_name'] = (string) $xmlObj->IDOC->E1EDL20->E1EDL28->E1EDL29->ROUTE_BEZ;
-          // carrier_tracking_number.
-          foreach ($xmlObj->IDOC->E1EDL20->E1EDL18 as $element) {
-            if ((string) $element->QUALF === 'PRO') {
-              $orders_details['carrier_tracking_number'][] = (string) $element->PARAM;
-              break;
-            }
-          }
-          foreach ($xmlObj->IDOC->E1EDL20->E1EDL37 as $element) {
-            if ($element->EXIDV2) {
-              $orders_details['carrier_tracking_number'][] = trim((string) $element->EXIDV2);
-            }
-          }
-          // material_number(Mostly SKU number)
-          // quantity_shipped.
-          foreach ($xmlObj->IDOC->E1EDL20->E1EDL24 as $element) {
-            $item_sku = trim((string) $element->MATNR);
-            $qty_shipped = trim((string) $element->LFIMG);
-            $orders_details['item_and_quantity'][$item_sku] = $qty_shipped;
-          }
-          // Calculate total items.
-          foreach ($orders_details['item_and_quantity'] as $sku => $qty) {
-            $orders_details['total_quantity'] = $orders_details['total_quantity'] + (int) $qty;
-          }
-          $this->createShipment($orders_details);
-        }
-
+      foreach ($xml_files as $xml_file) {
+        $operations[] = [
+          '\Drupal\commerce_order_customizations\BatchService::processOrderUpdate',
+        [$xml_file, $this->root],
+        ];
       }
-      catch (\Exception $e) {
-        $this->output()->writeln($e->getMessage());
-        return [];
-      }
-      $this->logger()->notice("Reading phase completed.");
-    }
-    return $data;
-  }
-
-  /**
-   * Creates shipments.
-   *
-   * @param array $orders_details
-   *   Array contains order contents.
-   */
-  public function createShipment($orders_details) {
-    $flag = 0;
-    try {
-      $shipment_query = $this->entityTypeManager->getStorage('commerce_shipment')->getQuery();
-      $shipment_id_arr = $shipment_query->condition('title', $orders_details['shipment_number'])
-        ->condition('field_sap_shipment', 1)
-        ->accessCheck(FALSE)
-        ->execute();
-
-      if (empty($shipment_id_arr)) {
-        $order_query = $this->entityTypeManager->getStorage('commerce_order')->getQuery();
-        // It should return single id as order number is unique.
-        $order_id_arr = $order_query->condition('order_number', $orders_details['order_number'])
-          ->accessCheck(FALSE)
-          ->execute();
-        $shipment_date = DrupalDateTime::createFromFormat('Ymd', $orders_details['shipment_date']);
-        $order_id = array_keys($order_id_arr);
-        // $order_obj = $this->entityTypeManager->getStorage('commerce_order')->load($order_id);
-        // $shipping_profile = $order_obj->get('shipments')->entity->getShippingProfile();
-        // Create a shipment entity.
-        $shipment = Shipment::create([
-        // You can choose the appropriate shipment type.
-          'type' => 'default',
-          'order_id' => $order_id[0],
-          'field_sap_shipment' => 1,
-          'field_sap_order_number' => $orders_details['sap_order_number'],
-          'field_shipment_date' => $shipment_date->format('Y-m-d'),
-          'field_total_item_quantity' => $orders_details['total_quantity'],
-          'field_item_and_quantity' => $this->createItemParagraph($orders_details),
-          'field_sap_shipment_tracking' => $this->createTrackingParagraph($orders_details),
-        // You can set the initial state as needed.
-          'state' => 'draft',
-        ]);
-        $shipment->setTitle($orders_details['shipment_number']);
-        $shipment->save();
-      }
-    }
-    catch (Exception $e) {
-      $this->output()->writeln($e->getMessage());
-    }
-  }
-
-  /**
-   * Creates Item and Quantity paragraph entity.
-   */
-  public function createItemParagraph($order_info) {
-    $item_info = $order_info['item_and_quantity'];
-    $paragraph_arr = [];
-    $data = [];
-    $i = 0;
-    foreach ($item_info as $sku => $qty) {
-      $paragraph_arr[$i] = Paragraph::create([
-        'type' => 'order_item_and_quantity',
-        'field_item_name' => $sku,
-        'field_item_quantity_shipped' => $qty,
+      batch_set([
+        'title' => t('Batch Processed Kuntal'),
+        'operations' => $operations,
+        'finished' => '\Drupal\commerce_order_customizations\BatchService::processUserUpdateFinished',
       ]);
-      $paragraph_arr[$i]->save();
-      $data[] = [
-        'target_id' => $paragraph_arr[$i]->id(),
-        'target_revision_id' => $paragraph_arr[$i]->getRevisionId(),
-      ];
-      $i = $i + 1;
-    }
-    return $data;
-  }
+      // Start the batch process.
+      drush_backend_batch_process();
+      $this->logger()->notice("Batch operations end.");
 
-  /**
-   * Creates SAP Shipment Tracking paragraph entity.
-   */
-  public function createTrackingParagraph($order_info) {
-    $item_info = $order_info['carrier_tracking_number'];
-    $paragraph_arr = [];
-    $data = [];
-    $i = 0;
-    foreach ($item_info as $item) {
-      $linkValue = 'https://www.ups.com/mobile/track?trackingNumber=' . $item;
-      $paragraph_arr[$i] = Paragraph::create([
-        'type' => 'sap_shipment_tracking',
-        'field_tracking_number' => $item,
-        'field_tracking_link' => ['uri' => $linkValue],
-      ]);
-      $paragraph_arr[$i]->save();
-      $data[] = [
-        'target_id' => $paragraph_arr[$i]->id(),
-        'target_revision_id' => $paragraph_arr[$i]->getRevisionId(),
-      ];
-      $i = $i + 1;
+      $this->logger()->success("Shipment Creation and Order Update Completed.");
     }
-    return $data;
   }
 
   /**
