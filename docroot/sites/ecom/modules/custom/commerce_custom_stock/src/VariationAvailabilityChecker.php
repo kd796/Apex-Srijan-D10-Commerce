e9121467ef4691f4548dc522f9c7844474ace5ad
Jayahar95 <jayahar@srijan.net>
2023-09-30 22:04:12 +0530

    APEX-000: out of stock add-to-cart changes

--- docroot/sites/ecom/modules/custom/commerce_custom_stock/src/VariationAvailabilityChecker.php
+++ docroot/sites/ecom/modules/custom/commerce_custom_stock/src/VariationAvailabilityChecker.php
@@ -25,16 +25,17 @@ class VariationAvailabilityChecker implements AvailabilityCheckerInterface {
   public function check(OrderItemInterface $order_item, Context $context) {
 
     $entity = $order_item->getPurchasedEntity();
-    $quantity = $entity->qty_increments ? $entity->qty_increments->value : 0;
-    // Order should not be placed if Stock is zero.
+    $quantity = (int) $order_item->getQuantity();
+    $stock_val = (int) $entity->field_stock->value;
+    // Order should not be placed if Stock is less than minimum quantity.
     $route_name = \Drupal::routeMatch()->getRouteName();
 
-    if ($entity->field_stock->value < $quantity) {
+    if ($stock_val < $quantity) {
       $result = t('This product is out of stock.');
       return AvailabilityResult::unavailable($result);
     }
     // Cart quantity should not be more than stock value and checking this only on cart page.
-    if ($route_name == 'commerce_cart.page' && $quantity > $entity->field_stock->value) {
+    if ($route_name == 'commerce_cart.page' && $quantity > $stock_val) {
       $result = t('The quantity is more than the stock quantity');
       return AvailabilityResult::unavailable($result);
     }
