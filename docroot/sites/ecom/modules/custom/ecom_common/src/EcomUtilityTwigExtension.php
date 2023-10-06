329b9ada5c733359944e43d68e8c58b2d0252cbb
Kuntal <kuntal.das@srijan.net>
2023-10-06 21:39:43 +0530

    APEX-000: Email template

--- docroot/sites/ecom/modules/custom/ecom_common/src/EcomUtilityTwigExtension.php
+++ docroot/sites/ecom/modules/custom/ecom_common/src/EcomUtilityTwigExtension.php
@@ -14,6 +14,7 @@ class EcomUtilityTwigExtension extends AbstractExtension {
    * Creates twig function for getting product name and image.
    */
   public function getFunctions() {
+
     return [
       new TwigFunction('get_product_name', [$this, 'getProductName']),
     ];
@@ -31,26 +32,14 @@ class EcomUtilityTwigExtension extends AbstractExtension {
       ]);
     $product_node = array_values($product_node);
     $product_name = $product_node[0]->get('field_long_description')->value;
-    $product_listing_image_uri = $this->getImageUri('field_media', $product_node[0], 'field_media_image');
-    $image_style = \Drupal::entityTypeManager()->getStorage('image_style')->load('tiny');
-    $url = $image_style->buildUrl($product_listing_image_uri);
+    $product_listing_image_uri = $product_node[0]->field_media->entity->field_media_image->entity->getFileUri();
+    $image_styled_uri = \Drupal::entityTypeManager()->getStorage('image_style')->load('tiny')->buildUrl($product_listing_image_uri);
     $product_name_image_arr = [
       'name' => $product_name,
-      'image_url' => $url,
+      'image_url' => $image_styled_uri,
     ];
 
     return $product_name_image_arr;
 
   }
-
-  /**
-   * Get image uri.
-   */
-  public function getImageUri($field_name, $entity, $media_field) {
-    $image_uri = $entity->$field_name->entity->$media_field->entity->getFileUri();
-    $url = !empty($image_uri) ? \Drupal::service('file_url_generator')->generateAbsoluteString($image_uri) : NULL;
-
-    return $url;
-  }
-
 }
